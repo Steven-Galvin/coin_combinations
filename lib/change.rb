02d57dd12d970ebc051ec9c70d400e8b6128454e
Grady Shelton and Steven Galvin <grady@>
2017-04-20 10:20:02 -0700

    pass test for returning dimes, nickles, and pennies.

--- lib/change.rb
+++ lib/change.rb
@@ -2,19 +2,43 @@ class Fixnum
   define_method(:change) do
       # pennies = 1
       nickels = 5
+      dimes = 10
 
       change = self
 
       pennies = (change % nickels)
 
-     nickel_change = (change - (change % nickels))
-      if nickel_change >=5
-        until nickels == nickel_change
-          nickels += 5
+      if change >= 10
+
+        dime_change = (change - (change % dimes))
+        if dime_change >= 10
+          until dimes == dime_change
+            dimes += 10
+          end
+        end
+
+        dime_remainder = change % dimes
+
+        nickel_change = (dime_remainder - (dime_remainder % nickels))
+        if nickel_change >=5
+          until nickels == nickel_change
+            nickels += 5
+          end
+        else
+          nickels = 0
         end
       else
-        nickels = 0
+        dimes = 0
+        nickel_change = (change - (change % nickels))
+        if nickel_change >=5
+          until nickels == nickel_change
+            nickels += 5
+          end
+        else
+          nickels = 0
+        end
       end
+
 
       #
       #
@@ -25,6 +49,7 @@ class Fixnum
       # (nickels / 5).to_s + ' nickels'
       penny_result = pennies.to_s
       nickel_result = (nickels / 5).to_s
-      nickel_result + " nickels and " + penny_result + " pennies."
+      dime_result = (dimes / 10).to_s
+      dime_result + " dimes, " + nickel_result + " nickels and " + penny_result + " pennies."
   end
 end
--- spec/change_spec.rb
+++ spec/change_spec.rb
@@ -3,10 +3,14 @@ require('change')
 
 describe('Fixnum#change') do
   it('will count up to desired change in pennies')do
-    expect(4.change).to(eq('0 nickels and 4 pennies.'))
+    expect(4.change).to(eq('0 dimes, 0 nickels and 4 pennies.'))
   end
 
   it('will count up to desired change in nickels and pennies')do
-    expect(22.change).to(eq('4 nickels and 2 pennies.'))
+    expect(7.change).to(eq('0 dimes, 1 nickels and 2 pennies.'))
+  end
+
+  it('will count up to desired change in dimes, nickels and pennies')do
+    expect(27.change).to(eq('2 dimes, 1 nickels and 2 pennies.'))
   end
 end
