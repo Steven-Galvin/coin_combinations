be92f56dcbf8d10fb1f9a1f68bc49253943bed85
Grady Shelton and Steven Galvin <grady@>
2017-04-20 10:04:11 -0700

    pass second test to display desired change in nickels and pennies

--- lib/change.rb
+++ lib/change.rb
@@ -1,20 +1,30 @@
 class Fixnum
   define_method(:change) do
-      pennies = 1
+      # pennies = 1
       nickels = 5
 
-    #  self - (self % nickels)
-        # until nickels == self
-        #   nickels += 5
-        # end
+      change = self
 
+      pennies = (change % nickels)
 
-
-      until pennies == self
-        pennies += 1
+     nickel_change = (change - (change % nickels))
+      if nickel_change >=5
+        until nickels == nickel_change
+          nickels += 5
+        end
+      else
+        nickels = 0
       end
 
+      #
+      #
+      # until pennies == self
+      #   pennies += 1
+      # end
+
       # (nickels / 5).to_s + ' nickels'
-      pennies.to_s + ' pennies'
+      penny_result = pennies.to_s
+      nickel_result = (nickels / 5).to_s
+      nickel_result + " nickels and " + penny_result + " pennies."
   end
 end
--- spec/change_spec.rb
+++ spec/change_spec.rb
@@ -3,14 +3,10 @@ require('change')
 
 describe('Fixnum#change') do
   it('will count up to desired change in pennies')do
-    expect(90.change).to(eq('90 pennies'))
+    expect(4.change).to(eq('0 nickels and 4 pennies.'))
   end
 
-  # it('will count up to desired change in nickels')do
-  #   expect(90.change).to(eq('18 nickels'))
-  # end
-  #
-  # it('will count up to desired change in nickels')do
-  #   expect(90.change).to(eq('18 nickels'))
-  # end
+  it('will count up to desired change in nickels and pennies')do
+    expect(22.change).to(eq('4 nickels and 2 pennies.'))
+  end
 end
