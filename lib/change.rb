ee85fcc962aa198303a5041511aacb7d5ad8dada
Grady Shelton and Steven Galvin <grady@>
2017-04-20 10:30:32 -0700

    pass test to account for quarters along with other coins

--- lib/change.rb
+++ lib/change.rb
@@ -1,13 +1,55 @@
 class Fixnum
   define_method(:change) do
-      # pennies = 1
-      nickels = 5
-      dimes = 10
+    change = self
+    nickels = 5
+    dimes = 10
+    quarters = 25
+    pennies = (change % nickels)
 
-      change = self
+    if change >= 25
+      quarter_change = (change - (change % quarters))
+      if quarter_change >=25
+        until quarters == quarter_change
+          quarters += 25
+        end
+      end
 
-      pennies = (change % nickels)
+      quarter_remainder = change % quarters
 
+
+
+      if quarter_remainder >= 10
+
+        dime_change = (quarter_remainder - (quarter_remainder % dimes))
+        if dime_change >= 10
+          until dimes == dime_change
+            dimes += 10
+          end
+        end
+
+        dime_remainder = quarter_remainder % dimes
+
+        nickel_change = (dime_remainder - (dime_remainder % nickels))
+        if nickel_change >=5
+          until nickels == nickel_change
+            nickels += 5
+          end
+        else
+          nickels = 0
+        end
+      else
+        dimes = 0
+        nickel_change = (change - (change % nickels))
+        if nickel_change >=5
+          until nickels == nickel_change
+            nickels += 5
+          end
+        else
+          nickels = 0
+        end
+      end
+    else
+      quarters = 0
       if change >= 10
 
         dime_change = (change - (change % dimes))
@@ -38,18 +80,12 @@ class Fixnum
           nickels = 0
         end
       end
+    end
 
-
-      #
-      #
-      # until pennies == self
-      #   pennies += 1
-      # end
-
-      # (nickels / 5).to_s + ' nickels'
       penny_result = pennies.to_s
       nickel_result = (nickels / 5).to_s
       dime_result = (dimes / 10).to_s
-      dime_result + " dimes, " + nickel_result + " nickels and " + penny_result + " pennies."
+      quarter_result = (quarters / 25).to_s
+      quarter_result + " quarters, " + dime_result + " dimes, " + nickel_result + " nickels and " + penny_result + " pennies."
   end
 end
--- spec/change_spec.rb
+++ spec/change_spec.rb
@@ -3,14 +3,18 @@ require('change')
 
 describe('Fixnum#change') do
   it('will count up to desired change in pennies')do
-    expect(4.change).to(eq('0 dimes, 0 nickels and 4 pennies.'))
+    expect(4.change).to(eq('0 quarters, 0 dimes, 0 nickels and 4 pennies.'))
   end
 
   it('will count up to desired change in nickels and pennies')do
-    expect(7.change).to(eq('0 dimes, 1 nickels and 2 pennies.'))
+    expect(7.change).to(eq('0 quarters, 0 dimes, 1 nickels and 2 pennies.'))
   end
 
   it('will count up to desired change in dimes, nickels and pennies')do
-    expect(27.change).to(eq('2 dimes, 1 nickels and 2 pennies.'))
+    expect(17.change).to(eq('0 quarters, 1 dimes, 1 nickels and 2 pennies.'))
+  end
+
+  it('will count up to desired change in dimes, nickels and pennies')do
+    expect(42.change).to(eq('1 quarters, 1 dimes, 1 nickels and 2 pennies.'))
   end
 end
